0da23c04d3dafd3d0679bedf3165dcf8334f2295
Neal Terrell <neal.terrell@csulb.edu>
2017-01-05 10:45:51 -0800

    Initial commit.

---
+++ test/Cecs475.War.Test/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Cecs475.War;
+using FluentAssertions;
+
+namespace Cecs475.War.Test {
+	public class WarPublicTests {
+		[Fact]
+		public void NewDeckTopCardIsAceOfHearts() {
+			Deck d = new Deck();
+			Card c = d.DealOne();
+			c.Kind.Should().Be(Card.CardKind.Ace, "Ace of Hearts is the top card of a new deck");
+			c.Suit.Should().Be(Card.CardSuit.Hearts, "Ace of Hearts is the top card of a new deck");
+		}
+	}
+}
